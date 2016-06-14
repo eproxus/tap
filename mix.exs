7a0c0083cc7d37d5780f2b4b4f0d6f5ce91cbf71
Adam Lindberg <hello@alind.io>
2016-06-14 16:16:26 +0200

    Bump to version 0.1.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Tap.Mixfile do
       app: :tap,
       description: "Elixir tracing",
       package: package,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
