dc8fdaaaaef5bf455005d43a3c27d8283252ac2a
Adam Lindberg <hello@alind.io>
2017-09-27 09:42:29 +0200

    Version 0.1.5

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Tap.Mixfile do
       app: :tap,
       description: "Elixir tracing",
       package: package(),
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
